4a4c37e9f25941b727a9e278ea9d97e0272f7232
Lerato Mokaloba <mokalobalerato@gmail.com>
2025-11-08 19:12:17 +0200

    Defined time interval values in seconds for use in relative time conversion

--- src/utils/DateUtils.js
+++ src/utils/DateUtils.js
@@ -31,4 +31,14 @@ export function getRelativeTime(dateString) {
   const date = new Date(dateString);
   const now = new Date();
   const diffInSeconds = Math.floor((now - date) / 1000);
+
+    const intervals = {
+    year: 31536000,
+    month: 2592000,
+    week: 604800,
+    day: 86400,
+    hour: 3600,
+    minute: 60
+  };
+  
 }
